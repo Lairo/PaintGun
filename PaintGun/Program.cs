602f579c62e6625ce8950b75b268d735b0f89165
chajus1 <chajus1@googlemail.com>
2024-09-25 06:48:17 +0100

    done some

--- PaintGun/Program.cs
+++ PaintGun/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PaintballGun gun = new();
+
+            while (true)
+            {
+                Console.WriteLine($"{gun.GetBalls()} balls; {gun.GetBallsLoaded()} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WARNINGL You're out of ammo");
+                Console.WriteLine("Space to shoot, r to reload, = to add ammo, q to quit");
+                char key = Console.ReadKey().KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '=') gun.SetBalls(gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+                else if (key == 'q') return;
+            }
         }
     }
 }
